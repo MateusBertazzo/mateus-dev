b9dd929ce1b114afd8803a18f124061a537ea11a
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-02 19:09:50 -0300

    .

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -20,14 +20,7 @@ export const Header = () => {
     return (
         <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
             <div className="container flex items-center justify-between">
-                <Link href="/">
-                    <Image
-                        width={58}
-                        height={58}
-                        src="/images/logo.svg"
-                        alt="logo-dev"
-                    />
-                </Link>
+                <span className="text-emerald-400">#</span>
 
                 <nav className="flex items-center gap-4 sm:gap-10">
                     {NAV_ITEMS.map(item => (
