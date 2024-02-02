d0b2d6e1b78085917ad1de2d914d5644b344a992
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-02 09:16:11 -0300

    cria componente header e estiliza o mesmo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
+import { Header } from './components/header'
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 })
-
 
 const plexMono = IBM_Plex_Mono({
   variable: '--font-plex-mono',
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
+        <Header />
         {children}
       </body>
     </html>
