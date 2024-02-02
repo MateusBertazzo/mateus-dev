a91917829a9b3a333adeff30da4df0e074962850
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-02 10:36:18 -0300

    cria sessão das tecnologias - button para entrar em contato

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,9 +1,23 @@
+import { Button } from "@/app/components/button"
 import { TechBadge } from "@/app/components/tech-badge"
 import Image from "next/image"
+import { HiArrowNarrowRight } from "react-icons/hi"
+import { TbBrandGithub, TbBrandLinkedin } from "react-icons/tb"
+
+const MOCK_CONTACTS = [
+    {
+        url: 'https://github.com/MateusBertazzo',
+        icon: <TbBrandGithub />,
+    },
+    {
+        url: 'https://www.linkedin.com/in/mateus-bertazzo-dev/',
+        icon: <TbBrandLinkedin />,
+    }
+]
 
 export const HeroSection = () => {
     return (
-        <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
+        <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
             <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
                 <div className="w-full lb:max-w-[530px]">
                     <p className="font-mono text-emerald-400">Olá, meu nome é</p>
@@ -21,15 +35,31 @@ export const HeroSection = () => {
                         ))}
                     </div>
 
-                    <div>
-                        Contato
+                    <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
+                        <Button className="w-max shadow-button">
+                            Entre em Contato
+                            <HiArrowNarrowRight />
+                        </Button>
+
+                        <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
+                            {MOCK_CONTACTS.map((contact, index) => (
+                                <a 
+                                    href={contact.url}
+                                    key={`contact-${index}`}
+                                    target="_blank"
+                                    className="hover:text-gray-100 transition-colors"
+                                    >
+                                        {contact.icon}
+                                </a>
+                            ))}
+                        </div>
                     </div>
 
                 </div>
 
                 <div>
                 <Image
-                    className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
+                    className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover border-2"
                     width={420}
                     height={404}
                     src="/images/profile-pic.png"
